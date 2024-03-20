cc24a2fee3559756eb099e8c10306c27f75b572f
Lucas Harada <lucasyharada@gmail.com>
2024-03-20 19:52:37 -0300

    Enable starship prompt

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -16,8 +16,10 @@
   programs.zsh.autosuggestion.enable = true;
   programs.zsh.syntaxHighlighting.enable = true;
   programs.zsh.history.extended = true;
-  programs.zsh.defaultKeymap = "vicmd";
+  programs.zsh.defaultKeymap = "viins";
   programs.zsh.shellAliases = {
     nixswitch = "darwin-rebuild switch --flake ${self}";
   };
+  
+  programs.starship.enable = true;
 }
